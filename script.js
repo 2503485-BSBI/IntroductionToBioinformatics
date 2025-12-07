a6c93fe63cbc1e3147ab48fdc9ed43b9a3d7112c
2503485-BSBI <2503485@students.au.edu.pk>
2025-12-07 11:02:24 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML =
+        '<div id="created">Account Created Sucessfully!</div><div id="refresh">Refresh the page to go back.</div>';
+
+    document.getElementById("created").style.display = "block";
+}
