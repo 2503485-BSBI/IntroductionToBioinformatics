8a07f03416051b055131aab8c9b1fc6bf01fc09d
2503485-BSBI <2503485@students.au.edu.pk>
2025-12-25 00:32:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML =
-        '<div id="created">Account Created Sucessfully!</div><div id="refresh">Refresh the page to go back.</div>';
 
-    document.getElementById("created").style.display = "block";
+function notification() {
+    alert("Account Created Sucessfully!");
 }
+
